3925bfc5824d9176833a7ac1110e5827c46a731a
Sindre <sndrem@live.no>
2017-11-18 23:17:04 +0100

    Added some persistence

--- scripts/uptime.js
+++ scripts/uptime.js
@@ -18,10 +18,18 @@ module.exports = function(bot) {
 		SITES.forEach(site => {
 			isReachable(site).then(reachable => {
 				const now = new Date()
-				if(reachable) {
-					bot.messageRoom(ROOM, `${site} is online at ${now}`);
+				const successFull = bot.brain.get("success");
+				if(reachable ) {
+					if(successFull >= 60) {
+						bot.messageRoom(ROOM, `${site} is online at ${now} and has been online for 60 minutes`);
+						bot.brain.set("success", 0)
+					} else {
+						bot.brain.set("success", successFull++);
+					}
+
 				} else {
 					bot.messageRoom(ROOM, `@sndrem :fire: ${site} is offline at ${now}. You should probably check it out :fire:`);
+					bot.brain.set("success", 0);
 				}
 			})
 		});
